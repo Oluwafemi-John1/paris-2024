c4847b3f4d665a1b1639169f6a9d838241b76123
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2024-07-24 11:06:06 +0100

    done

--- script.js
+++ script.js
@@ -21,12 +21,30 @@ function register() {
         allAthletes.push(person)
         console.log(allAthletes);
 
+        show.innerHTML = `
+            <tr>
+                <td>S/N</td>
+                <td>Athlete</td>
+                <td>Gender</td>
+                <td>Sport</td>
+                <td>Country</td>
+                <td>Actions</td>
+            </tr>
+        `
+
         for(var i = 0; i < allAthletes.length; i++) {
             show.innerHTML += `
-                <p>${allAthletes[i].athlete}</p>
-                <p>${allAthletes[i].gender}</p>
-                <p>${allAthletes[i].country}</p>
-                <p>${allAthletes[i].sport}</p>
+                <tr>
+                    <td>${i + 1}</td>
+                    <td>${allAthletes[i].athlete}</td>
+                    <td>${allAthletes[i].gender}</td>
+                    <td>${allAthletes[i].country}</td>
+                    <td>${allAthletes[i].sport}</td>
+                    <td>
+                        <button class="btn btn-danger">Delete</button>
+                        <button class="btn btn-warning">Edit</button>
+                    </td>
+                </tr>
             `
         }
     }
